6bf4df002fc376f23981dbedd235834dd6636e48
DaronP <jandresperez1@yahoo.com>
2021-08-31 15:37:15 -0600

    Segundo algoritmo implementado

--- client.py
+++ client.py
@@ -35,8 +35,8 @@ class SignUp(slixmpp.ClientXMPP):
 	def __init__(self, jid, password):
 		ClientXMPP.__init__(self, jid, password)
 
-		self.add_event_handler("session_start ", self.start)
-		self.add_event_handler("register ", self.register)
+		self.add_event_handler("session_start", self.start)
+		self.add_event_handler("register", self.register)
 
 	async def start(self, event):
 		self.send_presence()
@@ -51,11 +51,11 @@ class SignUp(slixmpp.ClientXMPP):
 		resp['register']['password'] = self.password
 		try:
 			await resp.send()
-			print("Account created for %s! " % self.boundjid)
+			print("Account created for %s!" % self.boundjid)
 			self.disconnect()
 
 		except IqError as e:
-			print("Could not register account: %s " %
+			print("Could not register account: %s" %
 					e.iq['error']['text'])
 			self.disconnect()
 			
@@ -85,7 +85,7 @@ class Client(ClientXMPP):
 		ClientXMPP.__init__(self, jid, password)
 		self.jid = jid
 		self.password = password
-		self.node = {'nodo': nodo, 'nodo_fuente': "", 'nodo_destino': "", 'saltos': 0, 'distancia': 0, 'mensaje': "", 'listado_nodos': listado_nodos, 'rec': False, 'sent': False, 'alg': 0}
+		self.node = {'nodo': nodo, 'nodo_fuente': "", 'nodo_destino': "", 'saltos': 0, 'distancia': 0, 'mensaje': "", 'listado_nodos': listado_nodos, 'alg': 0}
 
 		self.add_event_handler('session_start', self.session_start)
 		self.add_event_handler('message', self.message)
@@ -116,20 +116,19 @@ class Client(ClientXMPP):
 	#Funcion que recibe mensajes, tanto personales como grupales
 	def message(self, msg):
 		if msg['type'] == 'chat' or msg['type'] == 'normal':
-			print("***********Mensaje Recibido**************")
+			print("")
 			print("De: %(from)s \n %(body)s" %(msg))
-			print("*****************************************")
+			print("")
 
 			mens = msg['body'].split("/")
 			
 
-			self.node['nodo_fuente '] = mens[0].split(": ")[-1]
-			self.node['nodo_destino '] = mens[1].split(": ")[-1]
-			self.node['saltos '] = int(mens[2].split(": ")[-1])
-			self.node['distancia '] = int(mens[3].split(": ")[-1])
-			self.node['mensaje '] = mens[4].split(": ")[-1]
-			self.node['rec '] = True
-			self.node['alg '] = mens[5].split(": ")[-1]
+			self.node['nodo_fuente'] = mens[0].split(": ")[-1]
+			self.node['nodo_destino'] = mens[1].split(": ")[-1]
+			self.node['saltos'] = int(mens[2].split(": ")[-1])
+			self.node['distancia'] = int(mens[3].split(": ")[-1])
+			self.node['mensaje'] = mens[4].split(": ")[-1]
+			self.node['alg'] = mens[5].split(": ")[-1]
 			
 			#Aqui van los algoritmos
 			#1 = Flooding
@@ -138,16 +137,27 @@ class Client(ClientXMPP):
 
 			if (self.node['nodo_destino'] + "@alumchat.xyz") != self.node['nodo']:
 				self.node['saltos'] += 1
-				self.node['distancia'] += 1
+				
 
 				#Algoritmo Flooding en el lado de los nodos receptores
 				if self.node['alg'] == "1":
 					for i in self.node['listado_nodos']:
-						self.send_message(mto=i + "@alumchat.xyz", mbody="n_f: " + self.node['nodo_fuente'] + "/n_d: " + self.node['nodo_destino'] + "/s: " + self.node['saltos'] +"/d: "+ self.node['distancia'] +"/m: " + self.node['mensaje'] + "/alg: " + self.node['alg'], mtype='chat')
-						self.node['sent'] == True
+						self.node['distancia'] = self.node['distancia'] + int(i.split(" ")[-1])
+						self.send_message(mto=i.split(" ")[0] + "@alumchat.xyz", mbody="n_f: " + self.node['nodo_fuente'] + "/n_d: " + self.node['nodo_destino'] + "/s: " + str(self.node['saltos']) +"/d: "+ str(self.node['distancia']) +"/m: " + self.node['mensaje'] + "/alg: " + self.node['alg'], mtype='chat')
+			
+				if self.node['alg'] == "2":
+					dist_list = list()
+					for i in self.node['listado_nodos']:
+						dist_list.append(int(i.split(" ")[-1]))
+					
+					res = dist_list.index(min(dist_list))
+					
+					self.node['distancia'] = self.node['distancia'] + int(self.node['listado_nodos'].split(" ")[-1])
+					self.send_message(mto=self.node['listado_nodos'][res].split(" ")[0] + "@alumchat.xyz", mbody="n_f: " + self.node['nodo_fuente'] + "/n_d: " + self.node['nodo_destino'] + "/s: " + str(self.node['saltos']) +"/d: "+ str(self.node['distancia']) +"/m: " + self.node['mensaje'] + "/alg: " + self.node['alg'], mtype='chat')
+
 			
 			else:
-				print("\n\nMensaje recibido de: " + self.node['nodo_fuente'] + "\nMensaje: " + self.node['mensaje'] + "\n\n")
+				print("\n\n***********Mensaje Recibido**************\nDe: " + self.node['nodo_fuente'] + "\nMensaje: " + self.node['mensaje'] + "\n*****************************************\n\n")
 
 
 
@@ -164,17 +174,27 @@ class Client(ClientXMPP):
 		chat = True
 		while chat:			
 			
-			nd = await ainput("Ingrese el nodo destino: ")
-			mensaj = await ainput("Ingrese el mensaje: ")
-			alg = await ainput("Ingrese el algoritmo que desea ejecutar: 1. Flooding 2. Distance Vector Routing 3. ")
+			nd = await ainput("Ingrese el nodo destino \n")
+			mensaj = await ainput("Ingrese el mensaje \n")
+			alg = await ainput("Ingrese el algoritmo que desea ejecutar: 1. Flooding 2. Distance Vector Routing 3.  \n")
 
 			mnsg = "n_f: " + self.boundjid.bare + "/n_d: " + nd + "/s: 1/d: 0/m: " + mensaj + "/alg: " + alg
 			
 			#Algoritmo Flooding en el lado del nodo fuente
 			if alg == "1":
 				for i in self.node['listado_nodos']:
-					self.send_message(mto=i + "@alumchat.xyz", mbody=mnsg, mtype='chat')
-					self.node['sent'] == True
+					self.send_message(mto=i.split(" ")[0] + "@alumchat.xyz", mbody=mnsg, mtype='chat')
+			
+			if alg == "2":
+					dist_list = list()
+					for i in self.node['listado_nodos']:
+						dist_list.append(int(i.split(" ")[-1]))
+					
+					res = dist_list.index(min(dist_list))
+					
+					self.send_message(mto=self.node['listado_nodos'][res].split(" ")[0] + "@alumchat.xyz", mbody=mnsg, mtype='chat')
+
+			
 
 			
 			#8. Desconectarse
@@ -213,11 +233,11 @@ if __name__ == '__main__':
 			jid = jid + '@alumchat.xyz'
 			password = getpass.getpass("Password: ")
 
-			node_count = input("Ingrese el numero de nodos conectados a este: ")
+			node_count = input("Ingrese el numero de nodos conectados a este  \n")
 			node_list =[]
 
 			for i in range(int(node_count)):
-				node = input("Ingrese el nombre del nodo " + str(i) + " ")
+				node = input("Ingrese el nombre del nodo " + str(i+1) + " junto con la distancia separada por un espacio. \n")
 				node_list.append(node)
 
 			xmpp = Client(jid=jid, password=password, nodo=jid, listado_nodos=node_list)
